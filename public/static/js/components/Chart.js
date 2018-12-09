c4528b26078d818339082d4c24edb4afa51187a3
Shabanov Anton <snakesolid@ngs.ru>
2018-12-09 10:32:35 +0300

    display groups as inline blocks

--- public/static/js/components/Chart.js
+++ public/static/js/components/Chart.js
@@ -7,15 +7,25 @@ define([ "knockout", "d3", "Target", "Util", "bindings/GanttChart" ], function(k
 
 		this.groups = ko.observableArray([]);
 		this.selectedItems = ko.observableArray([]);
+		this.groupColors = ko.observable({});
 
 		this.hasSelectedItems = ko.pureComputed(function() {
 			return this.selectedItems().length > 0;
 		}, this);
 	};
 
+	Chart.prototype.groupStyle = function(name) {
+		const style = {};
+
+		style["background-color"] = this.groupColors()[name] || "#808080";
+
+		return style;
+	}
+
 	Chart.prototype.updateGroups = function(rows) {
 		const uniqueGroups = new Set();
 		const groups = [];
+		const groupColors = {};
 
 		for (const row of rows) {
 			uniqueGroups.add(row.groupName);
@@ -26,9 +36,11 @@ define([ "knockout", "d3", "Target", "Util", "bindings/GanttChart" ], function(k
 				name: name,
 				color: color,
 			});
+			groupColors[name] = color;
 		});
 
 		this.groups(groups);
+		this.groupColors = ko.observable(groupColors);
 	};
 
 	return Chart;
