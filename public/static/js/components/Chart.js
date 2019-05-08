1cf98583146efb273f1e2cf84bda37b42c708a36
Shabanov Anton <snakesolid@ngs.ru>
2019-05-08 08:01:30 +0300

    group duration and total execution time

--- public/static/js/components/Chart.js
+++ public/static/js/components/Chart.js
@@ -1,6 +1,6 @@
 "use strict";
 
-define(["knockout", "d3", "Target", "Util", "bindings/GanttChart"], function(ko, d3, Target, Util, GanttChart) {
+define(["knockout", "moment", "Target", "Util", "bindings/GanttChart"], function(ko, moment, Target, Util, GanttChart) {
 	const Chart = function(params) {
 		this.items = params.items;
 		this.items.subscribe(this.updateGroups.bind(this));
@@ -8,12 +8,21 @@ define(["knockout", "d3", "Target", "Util", "bindings/GanttChart"], function(ko,
 		this.groups = ko.observableArray([]);
 		this.selectedItems = ko.observableArray([]);
 		this.groupColors = ko.observable({});
+		this.duration = ko.observable();
 
 		this.hasItems = ko.pureComputed(function() {
 			return this.items().length > 0;
 		}, this);
 
-		this.hasSelectedItems = ko.pureComputed(function() {
+		this.hasDuration = ko.pureComputed(function() {
+			return this.duration() !== undefined;
+		}, this);
+
+		this.isGroupsVisible = ko.pureComputed(function() {
+			return this.selectedItems().length === 0;
+		}, this);
+
+		this.isSelectionVisible = ko.pureComputed(function() {
 			return this.selectedItems().length > 0;
 		}, this);
 	};
@@ -27,24 +36,50 @@ define(["knockout", "d3", "Target", "Util", "bindings/GanttChart"], function(ko,
 	};
 
 	Chart.prototype.updateGroups = function(rows) {
-		const uniqueGroups = new Set();
+		const groupStatistics = {};
 		const groups = [];
 		const groupColors = {};
+		let minTime = Number.POSITIVE_INFINITY;
+		let maxTime = Number.NEGATIVE_INFINITY;
+		let totalTime = 0;
 
 		for (const row of rows) {
-			uniqueGroups.add(row.groupName);
+			const name = row.groupName;
+			const duration = row.endTime - row.startTime;
+
+			if (name in groupStatistics) {
+				groupStatistics[name] += duration;
+			} else {
+				groupStatistics[name] = duration;
+			}
+
+			minTime = Math.min(row.startTime, minTime);
+			maxTime = Math.max(row.endTime, maxTime);
+			totalTime += duration;
 		}
 
-		Util.assignColors(uniqueGroups, function(name, color) {
+		Util.assignColors(Object.keys(groupStatistics), function(name, color) {
+			const groupTime = groupStatistics[name];
+			const duration = moment.duration(groupTime).humanize();
+			const percent = ((100.0 * groupTime) / totalTime).toFixed(1);
+
 			groups.push({
 				name: name,
 				color: color,
+				duration: duration,
+				percent: percent,
 			});
 			groupColors[name] = color;
 		});
 
 		this.groups(groups);
 		this.groupColors = ko.observable(groupColors);
+
+		if (maxTime > minTime) {
+			const duration = moment.duration(maxTime - minTime).humanize();
+
+			this.duration(duration);
+		}
 	};
 
 	return Chart;
