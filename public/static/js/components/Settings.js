e6227989e8c7de63514aa3898e1a9026d00c86f7
Shabanov Anton <snakesolid@ngs.ru>
2019-06-13 20:37:10 +0300

    refactoring settings table

--- public/static/js/components/Settings.js
+++ public/static/js/components/Settings.js
@@ -2,9 +2,9 @@
 
 define(["knockout", "Storage"], function(ko, Storage) {
 	function compareGroupsNames(a, b) {
-		if (a.fromName < b.fromName) {
+		if (a.groupName < b.groupName) {
 			return -1;
-		} else if (a.fromName > b.fromName) {
+		} else if (a.groupName > b.groupName) {
 			return 1;
 		} else {
 			return 0;
@@ -13,8 +13,8 @@ define(["knockout", "Storage"], function(ko, Storage) {
 
 	const Settings = function(params) {
 		this.groupMapping = ko.observableArray();
-		this.editGroupFromName = ko.observable();
-		this.editGroupToName = ko.observable();
+		this.editGroupName = ko.observable();
+		this.editRenameTo = ko.observable();
 
 		this.removePattern = function(item) {
 			this.groupMapping.remove(item);
@@ -27,8 +27,8 @@ define(["knockout", "Storage"], function(ko, Storage) {
 		const mappings = Storage.getGroupMapping();
 		const groupMapping = Object.keys(mappings).map(function(key) {
 			return {
-				fromName: key,
-				toName: ko.observable(mappings[key]),
+				groupName: key,
+				renameTo: ko.observable(mappings[key]),
 			};
 		});
 
@@ -39,13 +39,13 @@ define(["knockout", "Storage"], function(ko, Storage) {
 
 	Settings.prototype.addPattern = function() {
 		const groupMapping = this.groupMapping();
-		const fromName = this.editGroupFromName();
-		const toName = this.editGroupToName();
+		const groupName = this.editGroupName();
+		const renameTo = this.editRenameTo();
 		let nameExists = false;
 
 		for (const item of groupMapping) {
-			if (item.fromName === fromName) {
-				item.toName(toName);
+			if (item.groupName === groupName) {
+				item.renameTo(renameTo);
 				nameExists = true;
 
 				break;
@@ -54,8 +54,8 @@ define(["knockout", "Storage"], function(ko, Storage) {
 
 		if (!nameExists) {
 			groupMapping.push({
-				fromName: fromName,
-				toName: ko.observable(toName),
+				groupName: groupName,
+				renameTo: ko.observable(renameTo),
 			});
 			groupMapping.sort(compareGroupsNames);
 		}
@@ -65,7 +65,7 @@ define(["knockout", "Storage"], function(ko, Storage) {
 
 	Settings.prototype.saveSettings = function() {
 		const mappings = this.groupMapping().reduce(function(acc, item) {
-			acc[item.fromName] = item.toName();
+			acc[item.groupName] = item.renameTo();
 
 			return acc;
 		}, {});
